01d648523523e1ab3786ea8748e7208400e62f57
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-05-25 21:05:11 +0900

    Update Member.java

--- MyProject2/src/ch02/Member.java
+++ MyProject2/src/ch02/Member.java
@@ -7,23 +7,43 @@ public class Member {
 	int grade;
 	String email;
 	String address;
-	
-	Member() {}
+	String birthday;
 	
 	Member(String name, String phoneNo, String major, int grade, String email, String address) {
-		 this.name = "홍길동";
-		 this.phoneNo = "010-1010-000";
-		 this.major = "도적";
+		this.name = name;
+		this.phoneNo = phoneNo;
+		 this.major = major;
 		 this.grade = 100;
-		 this.email = "ninga@lekde.com";
-		 this.address = "한양 깊은산속";
+		 this.email = email;
+		 this.address = address;
+		 
+	}
+	
+	Member(String name, String phoneNo, String major, int grade, String email, String address, String birthday) {
+		this.name = name;
+		this.phoneNo = phoneNo;
+		 this.major = major;
+		 this.grade = 100;
+		 this.email = email;
+		 this.address = address;
+		 this.birthday = birthday;
 	}
 	public void MemberInfo() {
 		System.out.println("모든 정보를 출력합니다.");
+		System.out.println("이름 :" + name);
+		System.out.println("전화번호" + phoneNo);
+		System.out.println("학년 :" + grade);
+		System.out.println("이메일 주소 : " + email);
+		System.out.println("주소 :" + address);
+		System.out.println("생일 :" + birthday);
 	}
 	public static void main(String[] args) {
-		Member member = new Member();
+		Member member = new Member("연개소문", "000-0000-0000", "도적", 100, "sdfow@adf.com", "평양 어딘가");
+		member.MemberInfo();
 		
+		System.out.println();
+		Member member1 = new Member("홍길동", "010-1010-000", "도적", 100, "sdie@diene.com", "한양깊은산속", "07월01일");
+		member1.MemberInfo();
 		
 		
 	}
