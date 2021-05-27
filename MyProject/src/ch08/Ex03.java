0ecce06da064981023391e4f9d92479fcd4f0985
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-05-27 21:37:32 +0900

    Create Ex03.java

---
+++ MyProject/src/ch08/Ex03.java
@@ -0,0 +1,26 @@
+package ch08;
+
+public class Ex03 {
+	
+	public static void main(String[] args) {
+		int[][] arr = {	//3행 3열
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		};
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		for(int i = arr.length - 1; i >-1; i--) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
