7977f3eb078eb579e80e7861acdb4bb054a52250
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-05-31 17:50:44 +0900

    repair

---
+++ MyProject/src/ch10/Calculator.java
@@ -0,0 +1,9 @@
+package ch10;
+
+public interface Calculator {
+	long add(long n1, long n2);
+	long substract(long n1, long n2);
+	long multiply(long n1, long n2);
+	long divide(double n1, long n2);
+}
+
--- MyProject/src/ex01/HighFriend.java
+++ MyProject/src/ex01/HighFriend.java
@@ -1,27 +1,25 @@
-package ex.infomanager;
+package ex01;
 
-public class HighFriend extends Friend {
+public class HighFriend extends Friend{
 	
-	//고교친구는 직장 정보를 추가적으로 가진다.
+	//고교 친구는 직장 정보를 추가적으로 가진다.
 	String work;
 	
 	public HighFriend(String name, String phoneNumber, String address, String work) {
 		super(name, phoneNumber, address);
 		this.work = work;
-		
 	}
 
 	@Override
-	public void showData() {
+	public void showData() {		
 		super.showData();
-		System.out.println("직   업: " + work);
+		System.out.println("직   업:" + work);
 	}
 
 	@Override
 	public void showBasicInfo() {
 		System.out.println("이름 :" + name);
-		System.out.println("주소 :" + address);
+		System.out.println("전화 :" + phoneNumber);
 	}
 	
-
 }
