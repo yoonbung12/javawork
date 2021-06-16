1fd9423b6c4f5e66f491c8272419b1e5bf044ae4
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-16 10:04:44 +0900

    20210615_DDLTEST
    20210615_DDLTEST

--- sql/20210615_DDLTEST.sql
+++ sql/20210615_DDLTEST.sql
@@ -1,17 +1,16 @@
---첫번째 테이블 만들어보자
+--첫번째 테이블 만들어보자 
 --테이블 명 : phoneInfo_basic
 --컬럼 명: idx, fr_name, fr_phonenumber, fr_email, fr_address, fr_regdate
 create table phoneInfo_basic(
         idx number(6) constraint basic_idx_pk primary key,
-        
         fr_name varchar2(20) constraint basic_fr_name_nn not null,
-        
+      --fr_name varchar2(20) not null,
+      
         fr_phonenumber varchar2(20) constraint basic_fr_phonenumber_nn not null,
-        
+      --fr_phonenumber varchar(20) not null,  
+       
         fr_email varchar2(20),
-        
         fr_address varchar2(20),
-        
         fr_regdate Date default sysdate
 );
 select * from phoneInfo_basic;
@@ -24,23 +23,24 @@ create table phoneInfo_univ(
         fr_u_major varchar2(20) default 'N'
         constraint univ_fr_u_major_nn not null,
         
-        fr_u_year number(1) default  1
+        
+        fr_u_year number(1) default  1 
         constraint univ_year_nn not null
         constraint univ_year_ck check(0 < fr_u_year and fr_u_year <5),
-        
+                                         --fr_u_year between 1 and 4       
         fr_ref number(7) constraint univ_ref_fk references phoneInfo_basic(idx)
-);        
+); 
 desc phoneInfo_univ;        
 drop table phoneInfo_univ;        
 
 --phoneInfo_com 테이블 만들기
 create table phoneInfo_com (
         idx number(6) 
-        constraint pic_idx_pk primary key
-        constraint pic_idx_nn not null,
+        constraint pic_idx_pk primary key,
+        --constraint pic_idx_nn not null,
         
-        fr_c_company varchar2(20) default 'N'
-        constraint pic_fr_c_company_nn not null,
+        fr_c_company varchar2(20) default 'N' not null,
+        --constraint pic_fr_c_company_nn not null,
         
         fr_ref number(6) 
         constraint pic_fr_ref_fk references phoneInfo_basic(idx)
