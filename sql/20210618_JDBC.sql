c87043996fd00a5feceb89a3fd0bfb76a25aeccb
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-18 14:53:51 +0900

    JDBC.SQL
    JDBC.SQL

---
+++ sql/20210618_JDBC.sql
@@ -0,0 +1,17 @@
+--20210618
+
+--JDBC
+select * from dept01;
+
+insert into dept01 values(deptno01_deptno_seq.nextval, 'dev', 'SEOUL');
+
+-- dept01 테이블에 deptno 에 입력할 일련 번호 -> sequence
+create sequence dept01_deptno_seq
+start with 10
+INCREMENT by 10
+;
+
+delete from dept01 where deptno = ?;
+
+
+update dept01 set dname =?, loc=? where deptno=?;
