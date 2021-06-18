ce53f6690d590c06f9416996da3defda8318e2b1
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-18 17:42:05 +0900

    Update DeptMain.java

--- JDBCTest/src/dept/DeptMain.java
+++ JDBCTest/src/dept/DeptMain.java
@@ -1,30 +1,31 @@
 package dept;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class DeptMain {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
+		//Connection conn = null;
+		
+		DeptManager manager = new DeptManager(DeptDao.getInstance());
 		
 		//1.드라이버 로드
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("드라이버 로드 성골!");
+//			System.out.println("드라이버 로드 성골!");
+			manager.deptList();
 			
-			//2.연결
-			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe"; //몇번씩 해볼것
-			String user = "hr";
-			String pw = "tiger";
+			//2.연결 //DeptManager에서 사용하므로 없에도 괜찮음
 			
-			conn = DriverManager.getConnection(jdbcUrl, user, pw);
-			System.out.println("데이터베이스 연결 성공!!!");
+			//String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe"; //몇번씩 해볼것
+			//String user = "hr";
+			//String pw = "tiger";
+			
+			//conn = DriverManager.getConnection(jdbcUrl, user, pw);
+			//System.out.println("데이터베이스 연결 성공!!!");
 		
-			DeptDao dao = new DeptDao();
+			//DeptDao dao = DeptDao.getInstance();
 			
 //			List<Dept> list = dao.getDeptList(conn);
 //			
@@ -32,24 +33,24 @@ public class DeptMain {
 //				System.out.println(dept);
 //			}
 			
-			Scanner sc = new Scanner(System.in);
+			//Scanner sc = new Scanner(System.in);
 			
-			System.out.println("부서입력을 시작합니다.");
-			System.out.println("부서번호 부서이름 부서위치 순으로 입력해주세요.");
-			System.out.println("예) 50 dev seoul");
-			String deptData = sc.nextLine();
-			String[] deptDatas = deptData.split(" ");
-			
+//			System.out.println("부서입력을 시작합니다.");
+//			System.out.println("부서번호 부서이름 부서위치 순으로 입력해주세요.");
+//			System.out.println("예) 50 dev seoul");
+//			String deptData = sc.nextLine();
+//			String[] deptDatas = deptData.split(" ");
+//			
 			//Dept 객체 생성 -> Dao 메소드 호출
-			Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
+			//Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);
 			
-			int result = dao.insertDept(conn, dept);
-			if(result > 0) {
-				System.out.println("입력성공!");
-				
-			} else {
-				System.out.println("입력 실패");
-			}
+			//int result = dao.insertDept(conn, dept);
+//			if(result > 0) {
+//				System.out.println("입력성공!");
+//				
+//			} else {
+//				System.out.println("입력 실패");
+//			}
 			/*
 			//Delete
 			//System.out.println("부서 정보를 삭제합니다.");
@@ -64,13 +65,13 @@ public class DeptMain {
 			//}
 			 */
 		
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		
 		
