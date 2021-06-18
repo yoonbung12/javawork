185ff90ab2c4b678f7f8497c6d32f9653ff239b1
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-18 15:25:12 +0900

    Update DeptDao.java

--- JDBCTest/src/dept/DeptDao.java
+++ JDBCTest/src/dept/DeptDao.java
@@ -1,6 +1,7 @@
 package dept;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -14,11 +15,23 @@ public class DeptDao {
 		
 		ArrayList<Dept> list = null;
 		
-		//데이터 베이스의 테이블 이용 select 결과를 -> list에 저장
+		//데이터 베이스의 Dept테이블 이용 select 결과를 -> list에 저장
 		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
 			stmt = conn.createStatement();
+			String sql = "select * from dept order by deptno";
+			
+			//결과 받아오기
+			rs = stmt.executeQuery(sql);
+			
+			//데이터 Dept 객체로 생성 -> list에 저장
+			while(rs.next()) {
+				list.add(new Dept(rs.getInt(1), rs.getString(2), rs.getString(3)));
+			}
+			
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
