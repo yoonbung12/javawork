04f7fb46ae2173ac6bc9f792bfe9753c92046cae
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-18 16:00:39 +0900

    Update DeptDao.java

--- JDBCTest/src/dept/DeptDao.java
+++ JDBCTest/src/dept/DeptDao.java
@@ -1,6 +1,7 @@
 package dept;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -51,4 +52,47 @@ public class DeptDao {
 		
 		return list;
 	}
+
+	// 2. DEPT 테이블에 데이터 저장하는 메소드
+	// 반영 횟수 반환
+	// 사용자로 부터 데이터 받기 -> Dept 객체
+	int insertDept(Connection conn, Dept dept) {
+		
+		int result = 0;
+		
+		// 전달받은 Dept 객체의 데이터로 Dept 테이블에 저장 -> 결과 값을 반환
+		PreparedStatement pstmt = null;
+		
+
+		
+		try {
+			String sql = "insert into dept01 values(deptno01_deptno_seq.nextval, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dept.getDname());
+			pstmt.setString(2, dept.getLoc());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			
+			if(rs != null) {
+				rs.close();
+			}
+			
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
+
 }
