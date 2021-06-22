f6cff43d45fe0a35089c2efdf5dd64cce9a08aeb
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-22 12:39:21 +0900

    Update DeptDao.java

--- JDBCTest/src/dept/DeptDao.java
+++ JDBCTest/src/dept/DeptDao.java
@@ -9,6 +9,22 @@ import java.util.ArrayList;
 
 public class DeptDao {
 	
+	// 싱긅톤 패턴 : 여러개의 인스턴스를 생성하지 못하도록 하는 디자인 패턴
+	// 1. 외부 클래스 또는 인스턴스에서 해당 클래스로 인스턴스를 생성하지 못하도록 처리
+	private DeptDao() {
+		
+	}
+	
+	// 2. 클래스 내부에서 인스턴스를 만들고
+	static private DeptDao dao = new DeptDao();
+	
+	
+	// 3. 메소드를 통해서 반환 하도록 처리
+	public static DeptDao getInstance() {
+		return dao;
+	}
+	
+	
 	// 1. 전체 데이터 검색 기능
 	// 반환 타입 List<Dept> 
 	// 매개변수 - Connection 객체 : Statement
@@ -97,7 +113,42 @@ public class DeptDao {
 	// 3. Dept 테이블의 데이터 수정 메소드
 	// 반영된 행의 개수 반환
 	// 사용자로부터 데이터를 받아서 처리 -> Dept 객체
-	//int editDept(Connection conn, Dept dept) {
+//	int editDept(Connection conn, Dept dept) {
+											 
+	// 4. DEPT 테이블의 데이터를 삭제
+	// 삭제된 행의개수를 반환
+	// 사용자로부터 deptno 받아서 처리
+	int deleteDept(Connection conn, int deptno) {
 		
-	//}
+		int result = 0;
+		
+		
+		//  데이터 베이스 처리 sql
+		PreparedStatement pstmt = null;
+		String sql = "delete from dept where deptno = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, deptno);
+			
+			result = pstmt.executeUpdate();
+			
+			
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+	
 }
