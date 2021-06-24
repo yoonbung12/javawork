01c67537ffbf82d9955697da76cc3e8b646dd113
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-06-24 18:13:12 +0900

    Update 20210622_OopMiniProject.sql

--- sql/20210622_OopMiniProject.sql
+++ sql/20210622_OopMiniProject.sql
@@ -1,146 +1,180 @@
 --OopMiniProject
+--테이블 삭제
+drop table rent;
+drop table member;
+drop table car;
+drop table manager;
 
+--데이터 삭제
+delete from member;
+delete from car;
+delete from manager;
+delete from rent;
+--member table 생성
 create table member(
-membercode number(4) constraint member_mcode_pk primary key,
-id varchar2(20) not null,
+membercode number(4) constraint member_membercode_pk primary key,
+id varchar2(20) constraint member_id_uk unique not null,
 pw varchar2(20) not null,
 name varchar2(20) not null,
-carreg varchar2(14) not null,
+carreg varchar2(14) constraint member_carrg_uk unique not null,
 email varchar2(40) not null,
 address varchar2(40) not null
 )
 ;
+--car 테이블 생성
+create table car(
+carcode number(4) constraint car_carcode_pk primary key,
+carnumber varchar2(20) constraint car_cnum_uk unique not null,
+carname varchar2(20) not null,
+carsize varchar2(10) not null,
+carseat number(2) not null,
+caryear number(4) not null,
+fuel varchar2(20) not null,
+rent char(1) constraint car_rent_ck check(rent = '0' or rent = '1') 
+-- rent 는 대여현황을 체크하기 위한 컴럼으로 
+-- 0 이면 대여가 가능한 상태임을 나타내고 1이면 대여중임을 나타낸다
+)
+;
+--manager 테이블 생성
+create table manager(
+managercode number(4) constraint manager_managercode_pk primary key,
+mid varchar2(20) not null,
+mpw varchar2(20) not null
+)
+;
+
+create table rent(
+rentcode number(4) constraint rent_rentcode_pk primary key,
+pay number(6) not null,
+rentperiod number(1) constraint rent_period_ck check(rentperiod between 1 and 3)  not null,
+rent_date date default sysdate , --대여 날짜
+carcode number constraint rent_carcode_fk REFERENCES car(carcode) on delete cascade constraint rent_carcode_uk unique,
+membercode number constraint rent_membercode_fk REFERENCES member(membercode) on delete cascade constraint rent_membercode_uk unique,
+managercode number constraint rent_managercode_fk REFERENCES manager(managercode) on delete cascade
+)
+;
+
+select sysdate from dual;
+
+
+insert into rent values(rent_rentcode_seq.nextval,10000,3,sysdate+3,(select carcode from car where carnumber = 1111),(select membercode from member where carreg = 1111),1);
+select * from rent where 
+select to_char(rent_date, 'yyyy-mm-dd hh24:mi:ss') from rent;
+select * from rent;
+--데이터 출력
 desc member;
 select * from member;
+select * from car;
+--데이터 보기
+desc car;
+select * from car;
+
+--rent 데이터 보기
+desc rent;
+select * from rent;
+
+--member 시퀀스 생성
+CREATE SEQUENCE member_membercode_SEQ
+INCREMENT BY 1
+START WITH 1;
+-- member 시퀀스 삭제
+delete from member_membercode_seq;
+
+--car 시퀀스 생성
+CREATE SEQUENCE car_carcode_SEQ
+INCREMENT BY 1
+START WITH 1;
+-- car 시퀀스 삭제
+drop sequence car_carcode_seq;
+
+--rent 시퀀스 생성
+CREATE SEQUENCE rent_rentcode_SEQ
+INCREMENT BY 1
+START WITH 1;
+--rent 시퀀스 삭제
+drop sequence rent_rentcode_seq;
+
+--id 컬럼 삭제
+alter table member
+drop column id;
+
+alter table member
+add unique (id);
+
+--member 데이터 입력
+insert into member values
+(1, 'member2', '123456', '흥민', '22222', 'member1@naver.com', 'SEOUL');
+--member 데이터 삭제
+delete from member;
+-- member 테이블 삭제
+drop table member;
+
+
+insert into car values(CAR_CARCODE_SEQ.nextval, '2222', 'RAY', 'big', 5, 2018, '가솔린','1');
+insert into car values(CAR_CARCODE_SEQ.nextval, '4444', 'lambo', 'small', 2, 2021, '디젤','0');
+--car 데이터 삭제
+delete from car;
+
+
+
+-- 데이터 보기
+desc manager;
+select * from manager;
+--데이터 입력
+insert into manager values(1, 'admin', '1234');
+--manager 시퀀스 생성
+CREATE SEQUENCE manager_mncode_SEQ
+INCREMENT BY 1
+START WITH 1;
+--manager 시퀀스 삭제
+drop sequence manager_mncode_seq;
+
+-----------------------------------------------------
+
+--rent 테이블 생성 
+
+
+--데이터 저장
+insert into rent values(rent_rentcode_seq.nextval,10000,2,car_carcode_seq.nextval,member_mcode_seq.nextval,1 );
+--테이블 삭제
+drop table rent;
+--foreign key 삭제하고 
+alter table rent drop constraint rent_ccode_fk;
+alter table rent drop constraint rent_memcode_fk;
+alter table rent drop constraint rent_manacode_fk;
+--on delete cascade 제약사항 추가 sql
+alter table rent add constraint rent_ccode_fk FOREIGN key (carcode) REFERENCES car(carcode) on delete cascade;
+alter table rent add constraint rent_memcode_fk FOREIGN key (membercode) REFERENCES member(membercode) on delete cascade;
+alter table rent add constraint rent_manacode_fk FOREIGN key (managercode) REFERENCES manager(managercode) on delete cascade;
+
+
+
+
+
+
+
+
+
+--커밋
+commit;
+
+
+
+
+
+
+
+
+
+
+
+--sequence
 
 CREATE SEQUENCE member_membercode_SEQ
 INCREMENT BY 1
 START WITH 1;
 
-insert into member values
-(1, 'member1', '123456', '손흥민', '123456-1234567', 'member1@naver.com', 'SEOUL');
-insert into member values
-(2, 'member2', '123456', '케인', '234567-1345678', 'member2@google.com', 'PUSAN');
-insert into member values
-(3, 'member3', '123456', '모우라', '345678-1456789', 'member3@daum.net', 'DAEJEON');
-insert into member values
-(4, 'member4', '123456', '알리', '456789-1567890', 'member4@naver.com', 'JEJU');
-insert into member values
-(5, 'member5', '123456', '베일', '567890-1678901', 'member5@google.com', 'SEOUL');
 
-commit;
-create table car(
-carcode number(4) constraint car_carcode_pk primary key,
-carnumber varchar2(20) not null,
-carname varchar2(20) not null,
-carsize varchar2(10) not null,
-carseat number(2) not null,
-caryear number(4) not null,
-fuel varchar2(20) not null
-);
-
-desc car;
-select * from car;
-
-insert into car values(1, '1111', 'RAY', 'SMALL', 5, 2017, '가솔린');
-insert into car values(car_carcode_seq.nextval, '2222', 'NIRO EV', 'SMALL', 5, 2022, '전기');
-insert into car values(3, '3333', 'SONATA', 'MIDDLE', 5, 2020, '가솔린');
-insert into car values(4, '4444', 'MODEL3', 'MIDDLE', 5, 2020, '전기');
-insert into car values(5, '5555', 'BMW X7', 'BIG', 7, 2020, '가솔린');
-insert into car values(6, '6666', 'CARNIVAL', 'BIG', 9, 2021, '가솔린');
-
-CREATE SEQUENCE car_carcode_SEQ
-INCREMENT BY 1
-START WITH 1;
-select * from car;
-
-select To_Char(rent_date + 3, 'yyyymmdd')from dual;
-select to_date(sysdate,yy/mm/dd)
-from dual
-;
-
-select date, rent_date + 1 from dual;
-
-create table manager(
-managercode number(4) constraint manager_managercode_pk primary key,
-mid varchar2(20) not null,
-mpw varchar2(20) not null
-);
-
-desc manager;
-select * from manager;
-
-insert into manager values(1, 'manager1', '123123');
-insert into manager values(2, 'manager2', '321321');
-
-
-CREATE SEQUENCE manager_managercode_SEQ
-INCREMENT BY 1
-START WITH 1;
-
------------------------------------------------------
-create table rent(
-rentcode number(4) constraint rent_rentcode_pk primary key, --대여 번호
-pay INTEGER not null,  --가격지불
-rentperiod number(1) not null, --대여기간
-rentdate  date default sysdate, --대여 날짜
-carcode number constraint rent_carcode_fk REFERENCES car(carcode),
-membercode number constraint rent_memcode_fk REFERENCES member(membercode),
-managercode number constraint rent_managercode_fr REFERENCES manager(managercode)
-);
-
-
-select * from rent;
-Alter table rent ADD enddate date;
-
----- Test 반납일
-create table rentday (
-rentday1 number(1) not null,
-rentday2 number(2) not null,
-rentday3 number(3) not null
-);
-select * from rentday;
-insert into rentday values(1, 2, 3);
-
-
-
-
-insert into rent values ( 1, 20, 2, '20210202', 1, 1, 1, '20210204');
-insert into rent values ( 2, 22, 2, '20210201', 1, 1, 1, '20210203');
-commit;
-
-select dummy from dual where enddate > To_date('20210623', 'yyyymmdd');
-
-
-
-alter table rent add enddate (renperiod+date);
-select * from rent_view order by rentcode;
-select * from rent;
-select TO_Char(date + 1, 'yyyy/mm/dd') from dual;
-
-
-CREATE SEQUENCE rent_rentcode_SEQ
-INCREMENT BY 1
-START WITH 1;
-
-insert into rent values(1, 1000, 2, '2021/06/21', 1, 1 , 1);
-commit;
-
-desc rent;
-select * from rent;
-
-CREATE SEQUENCE rent_idx_SEQ
-INCREMENT BY 1
-START WITH 1;
-
-create table rent_order (
-order_num number(20) primary key,
-order_membercode number(4) references member(membercode) on delete cascade,
-order_carname varchar2(20),
-order_date date,
-order_time varchar2(20)
-);
-commit;
 --view
 create or replace view rent_view
 as
@@ -153,5 +187,5 @@ select * from rent_view
 order by rentcode;
 commit;
 
-insert into rent_view values(Rent_rentcode_SEQ.nextval,1001,3,'20210622', car_carcode_SEQ.currval, 1,member_membercode_SEQ.currval);
+
 -----------------------------------------------------
