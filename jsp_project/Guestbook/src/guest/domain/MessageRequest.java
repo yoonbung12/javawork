ad9fde339f1b4f634a5c4803a34aec7b0ead54f1
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-07-27 11:47:34 +0900

    beans 만듬
    beans 만듬

--- jsp_project/Guestbook/src/guest/domain/MessageRequest.java
+++ jsp_project/Guestbook/src/guest/domain/MessageRequest.java
@@ -1,5 +1,39 @@
 package guest.domain;
 
-public class MessageRequest {
+public class MessageRequest { //beans
 
+	private String guestname;
+	private String password;
+	private String message;
+	
+	
+	public String getGuestname() {
+		return guestname;
+	}
+	
+	public void setGuestname(String guestname) {
+		this.guestname = guestname;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	// MessageRequest => Message
+	public Message toMessage() {
+		return new Message(guestname, password, message);
+	}
+	
 }
