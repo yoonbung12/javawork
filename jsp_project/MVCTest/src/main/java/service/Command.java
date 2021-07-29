70bc147c3ffda6b36ebcf65169cf478ee981e3c1
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-07-29 20:43:24 +0900

    MVC
    MVC

---
+++ jsp_project/MVCTest/src/main/java/service/Command.java
@@ -0,0 +1,14 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface Command {
+	
+	
+	// GreetingCommandImpl
+	// 메소드가 추상메소드
+	// public abstract
+	String getPage(HttpServletRequest request);
+	
+	
+}
---
+++ jsp_project/MVCTest/src/main/java/service/InvalidCommandImpl.java
@@ -0,0 +1,17 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class InvalidCommandImpl implements Command {
+
+	@Override
+	public String getPage(HttpServletRequest request) {
+		
+		//..... 핵심처리
+		
+		return "/WEB-INF/views/default.jsp";
+
+		
+	}
+
+}
