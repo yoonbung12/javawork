ebc543766ceeced75e73afb93bd2fd9a95619925
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-11 17:45:28 +0900

    SpringJdbcTemplate
    SpringJdbcTemplate

--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/dao/MemberRowMapper.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/dao/MemberRowMapper.java
@@ -11,14 +11,17 @@ public class MemberRowMapper implements RowMapper<Member> {
 
 	@Override
 	public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		
 		Member member = new Member();
+		
 		member.setIdx(rs.getInt("idx"));
 		member.setMemberid(rs.getString("memberid"));
 		member.setPassword(rs.getString("password"));
 		member.setMembername(rs.getString("membername"));
 		member.setRegdate(rs.getTimestamp("regdate"));
 		
-		return null;
+		return member;
 	}
 
 	
