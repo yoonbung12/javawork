84ec93c01c41e173f445f1e0457c89d94a6a387d
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-14 14:20:51 +0900

    member-Manager project(Spring MVC)
    member-Manager project(Spring MVC)

--- spring_project(m)/Member-Manager/src/main/java/com/bitcamp/mm/controller/DelController.java
+++ spring_project(m)/Member-Manager/src/main/java/com/bitcamp/mm/controller/DelController.java
@@ -1,13 +1,25 @@
 package com.bitcamp.mm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bitcamp.mm.service.DelService;
 
 @Controller
 public class DelController {
 
-//	@Autowired
-//	DelService delService;
+	@Autowired
+	DelService delService;
 	
+	@RequestMapping(value = "/member/del/{id}")
+	public String dispatchDelView(HttpServletRequest request, @PathVariable String id) {
+			delService.delMember(request, id);
+			
+			return "member/delView";
+	}
 	
 }
