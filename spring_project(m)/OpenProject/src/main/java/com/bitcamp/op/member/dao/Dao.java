cf42a64f299c9285109be07c568f28e9928ae73d
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-14 13:46:08 +0900

    spring
     spring

--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/dao/Dao.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/dao/Dao.java
@@ -1,9 +1,18 @@
 package com.bitcamp.op.member.dao;
 
+import java.util.List;
+
 import com.bitcamp.op.member.domain.Member;
+import com.bitcamp.op.member.domain.SearchType;
 
 public interface Dao {
 
 	Member selectByIdPw(String id, String pw);
 	int selectById
+	
+	
+	
+	
+	// 검색을 통한 회원 리스트 8/13
+	List<Member> selectMember(SearchType searchType);
 }
--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberListService.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberListService.java
@@ -1,5 +1,15 @@
 package com.bitcamp.op.member.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MemberListService {
 
+	private Dao dao;
+	
+	@Autowired
+	private SqlSessionTemplate template;
+	
+	public List<Member> getMemberList(SearchType searchType) {
+		
+	}
 }
