8acf6ff93ce3f89292e4e86bf5d545e83651459a
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-16 18:50:15 +0900

    Mybatis 검색
    Mybatis 검색

--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberListService.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberListService.java
@@ -1,7 +1,16 @@
 package com.bitcamp.op.member.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bitcamp.op.member.dao.Dao;
+import com.bitcamp.op.member.domain.Member;
+import com.bitcamp.op.member.domain.SearchType;
+
+@Service
 public class MemberListService {
 
 	private Dao dao;
@@ -9,7 +18,14 @@ public class MemberListService {
 	@Autowired
 	private SqlSessionTemplate template;
 	
-	public List<Member> getMemberList(SearchType searchType) {
-		
+	public List<Member> getMemberList(){
+			return template.getMapper(Dao.class).selectAll();
+			
 	}
+	
+	public List<Member> getMemberList(SearchType searchType){
+			return template.getMapper(Dao.class).selectMember(searchType);
+	}
+	
+	
 }
