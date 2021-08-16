910b270782be1413d3096d52a0986d7ab88fd18f
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-16 18:29:05 +0900

    Spring + mybatisDao interface
    Spring + mybatisDao interface

--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/IdCheckService.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/IdCheckService.java
@@ -1,13 +1,9 @@
 package com.bitcamp.op.member.service;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bitcamp.op.jdbc.ConnectionProvider;
 import com.bitcamp.op.member.dao.Dao;
 import com.bitcamp.op.member.dao.JdbcTemplateMemberDao;
 import com.bitcamp.op.member.dao.MemberDao;
@@ -16,42 +12,35 @@ import com.bitcamp.op.member.dao.mybatisMemberDao;
 @Service
 public class IdCheckService {
 
-//	@Autowired
-//	MemberDao dao;
-	
-//	@Autowired
-//	private JdbcTemplateMemberDao dao;
-	
-//	@Autowired
-//	private mybatisMemberDao dao;
-	
-	private Dao dao;
-	
-	@Autowired
-	private SqlSessionTemplate template;
-	
-	public String idCheck(String id) {
 		
-		String result = "Y";
-		// Connection conn = null;
+//		@Autowired
+//		MemberDao dao;
+	
+//		@Autowired
+//		private JdbcTemplateMemberDao dao;
+	
+//		@Autowired
+//		private mybatisMemberDao dao;
+	
+		private Dao dao;
 		
-		// 인터페이스의 메퍼 DAO 생성
-		dao = template.getMapper(Dao.class);
+		@Autowired
+		private SqlSessionTemplate template;
 		
-		
-		try {
-
-				//conn = ConnectionProvider.getConnection();
-				if(dao.selectById(id)>0) {
-					result = "N";
-				}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		public String idCheck(String id) {
+			
+			String result= "Y";
+			//Connection conn = null;
+			
+			// 인터페이스의 메퍼 DAO 생성
+			dao = template.getMapper(Dao.class);
+			
+			//conn = ConnectionProvider.getConnection();
+			if(dao.selectById(id)>0) {
+					result="N";
+			}
+			return result;
+			
 		}
-		return result;
-
-	}
-	
-	
 	
 }
