4b2e4fedfbbb682fea8e3e77da0334fb37edc0c0
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2021-08-16 19:18:56 +0900

    spring + myBatis Dao interface
    spring + myBatis Dao interface

--- spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberRegService.java
+++ spring_project(m)/OpenProject/src/main/java/com/bitcamp/op/member/service/MemberRegService.java
@@ -1,19 +1,16 @@
 package com.bitcamp.op.member.service;
 
 import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.bitcamp.op.jdbc.ConnectionProvider;
-import com.bitcamp.op.jdbc.JdbcUtil;
-import com.bitcamp.op.member.dao.MemberDao;
-import com.bitcamp.op.member.dao.mybatisMemberDao;
+import com.bitcamp.op.member.dao.Dao;
 import com.bitcamp.op.member.domain.Member;
 import com.bitcamp.op.member.domain.MemberRegRequest;
 
@@ -25,146 +22,115 @@ public class MemberRegService {
 	//@Autowired
 	//private MemberDao dao;
 	
-	@Autowired
-	private mybatisMemberDao dao;
+	//@Autowired
+	//private mybatisMemberDao dao;
 
+	//@Autowired
+	//private mybatisMemberDao dao;
 	
-	public int  memberReg(
-			MemberRegRequest regRequest,
-			HttpServletRequest request
-			
-			) {
-		
-		
+	private Dao dao;
+	
+	@Autowired
+	private SqlSessionTemplate template;
+	
+	public int memberReg(MemberRegRequest regRequest, HttpServletRequest request) {
 		
 		int resultCnt = 0;
-		//Connection conn  = null;
+		// Connection conn = null;
 		File newFile = null;
-		
+	
 		try {
-		// 1.파일 저장
-			
-		// Member 객체 생성 -> 저장된 파일의 이름을 set
-		Member member = regRequest.toMember();
-		
-		// 파일 저장
-		if(regRequest.getPhoto() != null && !regRequest.getPhoto().isEmpty()) {
-			
-			
-			// 새로운 저장 폴더 : File
-			File newDir = new File(request.getSession().getServletContext().getRealPath(UPLOAD_URI));
-			
-			// 폴더가 존재하지 않으면 폴더 생성
-			if(!newDir.exists()) {
-				newDir.mkdir();
-				System.out.println("저장 폴더를 생성했습니다.");
+				// 1. 파일 저장
 				
-			}
-			
-			// 파일 저장시에 파일 이름이 같으면 덮어쓴다 -> 회원별 고유한 파일 이름을 만들자!!, 새로운 파일 이름에 확장자 추가
-			String newFileName = regRequest.getMemberid()
-					+ System.currentTimeMillis()
-					+ chkFileType(regRequest.getPhoto());
-			
-			// cool123123893845
-			
-			// 새로운 File 객체
-			newFile = new File(newDir, new FileName);
-			
-			regRequest.getPhoto().transferTo(newFile);
-			member.setMemberphoto(newFileName);
-			
-		} else {
-			member.setMemberphoto("photo.png");
-		}
-		
-		// 2. dao저장
-		// conn = ConnectionProvider.getConnection();
-		
-		resultCnt = dao.insertMember1(member);
-		
-		System.out.println("새롭게 등록된 idx => " + member.getIdx());
-		
-		// idx 값은 자식 테이블의 insert 시 외래키로 사용
-		
-		// 자식테이블 insert 구문.....
-		
-	} catch(IllegalStateException | IOException e) {
-		e.printStackTrace();
-	} catch(SQLException e) {
-			//DB 예외 발생 시 -> 저장된 파일을 삭제
-			if(newFile != null && newFile.exists()) {
-				newFile.delete();
+				// Member 객체 생성 -> 저장된 파일의 이름을 set
+				Member member = regRequest.toMember();
 				
-	}
-			e.printStackTrace();
-	} catch(Exception e) {
-		System.out.println(e.getMessage());
-		e.printStackTrace();
-	}
-		
-		// 시스템 경로
-		String path = request.getSession().getServletContext().getRealPath(UPLOAD_URI);
-		// 새로운 저장 폴더 : File
-		File newDir = new File(path);
-		
-		// 폴더가 존재하지 않으면 폴더 생성
-		if(newDir.exists()) {
-			newDir.mkdir();
-			System.out.println("저장 폴더를 생성했습니다.");
-		}
-		// 파일 저장시에 파일 이름이 같으면 덮어쓴다 ->  회원별 고유한 파일 이름을 만들자!!
-		String newFileName = regRequest.getMemberid()+System.currentTimeMillis(); 		
-		// cool123123451235412
-		
-		
-		// 새로운 File 객체를필요로함
-		 newFile = new File(newDir, newFileName);
-		
-		// 파일 저장
-		if(regRequest.getPhoto() != null && !regRequest.getPhoto().isEmpty())	{
-			regRequest.getPhoto().transferTo(newFile);
-		}
-		
-		// 2. dao 저장
-		conn = ConnectionProvider.getConnection();
-		
-		//  Member 객체 생성 -> 저장된 파일의 이름을 set
-		Member member = regRequest.toMember();
-		member.setMemberphoto(newFileName);
-		
-		resultCnt = dao.insertMember(conn, member);
-		
-		System.out.println("새롭게 등록된 idx => " + member.getIdx());
-		
-		// idx 갑은 자식 테이블의 insert 시 외래키로 사용
-		
-		// 자식테이블 insert 구문...
-		
-		
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  catch(SQLException e) {
-			//DB 예외 발생 시 -> 저장된 파일을 삭제
-			if(newFile != null && newFile.exists()) {
-				newFile.delete();
-			}
-			e.printStackTrace();
-		} finally {
-			JdbcUtil.close(conn);
-		}
+				// 파일 저장
+				if(regRequest.getPhoto() != null && !regRequest.getPhoto().isEmpty()) {
+						
+					// 새로운 저장 폴더 : File
+					File newDir = new File(request.getSession().getServletContext().getRealPath(UPLOAD_URI));
+					
+					// 폴더가 존재하지 않으면 폴더 생성
+					if(!newDir.exists()) {
+						newDir.mkdir();
+						System.out.println("저장 폴더를 생성했습니다.");
+					}
+					// 파일 저장시에 파일 이름이 같으면 덮어쓴다 -> 회원별 고유한 파일 이름을 만들자!!!, 새로운 파일이름에 확장자 추가
+					String newFileName = regRequest.getMemberid()
+									+ System.currentTimeMillis()
+									+ chkFileType(regRequest.getPhoto());
+					
+					// cool1231238936798123987
+					
+					// 새로운 File 객체
+					newFile = new File(newDir, newFileName);
+					
+					regRequest.getPhoto().transferTo(newFile);
+					member.setMemberphoto(newFileName);
+				} else {
+						member.setMemberphoto("photo.png");
+				}
+				
+				// 2. dao 저장
+				// conn = ConnectionProvider.getConnection();
+				
+				dao = template.getMapper(Dao.class);
+				
+				resultCnt = dao.insertMember(member);
+				
+				System.out.println("새롭게 등록된 idx => " + member.getIdx());
+				
+				// idx 값은 자식 테이블의 insert 시 외래키로 사용
+				
+				// 자식테이블 insert 구문....
+				
 			
-		
-		
-		
-		
-		
-		
-		
+		} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+		} catch (SQLException e) {
+				// DB 예외 발생 시 -> 저장된 파일을 삭제
+				if(newFile != null && newFile.exists()) {
+					newFile.delete();
+				}
+				e.printStackTrace();
+		} catch(Exception e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+		}
 		return resultCnt;
 	}
+	
+	
+	// 파일의 ContentType과 파일 확장자를 체크
+	private String chkFileType(MultipartFile file) throws Exception {
+			String extension = "";
+			
+			// 업로드 파일의 contentType
+			String contentType = file.getContentType();
+			if(!(contentType.equals("image/jpeg") || contentType.equals("image/jpg") || contentType.equals("image/png") || contentType.equals("image/gif"))) {
+					throw new Exception("허용하지 않는 파일 타입 :" + contentType);
+	
+	}
+			
+			// 파일 확장자 구하기
+			String fileName = file.getOriginalFilename();
+			
+			// String[]	java.lang.String.split(String regex)
+			// : 정규식의 패턴 문자열을 전달해야하기 때문에 \\. 으로 처리
+			String[] nameTokens = fileName.split("\\."); // tet.mini2.jpg PNG png
+			
+			// 토큰의 마지막 index의 문자열을 가져옴 : 배열의 개수 -1
+			extension = nameTokens[nameTokens.length - 1].toLowerCase();
+			
+			// 이미지 파일 이외의 파일 업로드 금지
+			// 파일 확장자 체크
+			if(!(extension.equals("jpg") || extension.equals("png") || extension.equals("gif"))) {
+					throw new Exception("허용하지 않는 파일 확장자 타입 : " + contentType);
+					
+			}
+			
+			return extension;
+	}
 }
+	
